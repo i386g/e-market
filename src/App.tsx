764e88b0130981544896d25a73cb2c63abff7222
josh.i386g <josh.i386g@proton.me>
2024-10-04 20:43:36 +0800

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { IconSearch } from "@tabler/icons-react";
-
 import "./App.css";
 
 import {
@@ -27,7 +25,24 @@ function App() {
             </Stack>
             <TextInput
               placeholder="Search here..."
-              leftSection={<IconSearch size={20} />}
+              leftSection={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="icon icon-tabler icons-tabler-outline icon-tabler-search"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+                  <path d="M21 21l-6 -6" />
+                </svg>
+              }
               radius="md"
               size="md"
             />
